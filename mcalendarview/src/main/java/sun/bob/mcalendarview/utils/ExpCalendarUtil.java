ce44e077485ec9e27ba331b09e4efa1ac4e60683
Cristopher Lizame <boxgamebig@gmail.com>
2019-12-16 06:14:28 -0500

    Update ExpCalendarUtil.java

--- mcalendarview/src/main/java/sun/bob/mcalendarview/utils/ExpCalendarUtil.java
+++ mcalendarview/src/main/java/sun/bob/mcalendarview/utils/ExpCalendarUtil.java
@@ -25,7 +25,7 @@ public class ExpCalendarUtil {
         } else {
             number = number + 1;
         }
-        final String[] shortWeekdaysxx = = new String[]{"Dom","Lun","Mar","Mier","Jue","Sab"};
+        final String[] shortWeekdaysxx = new String[]{"Dom","Lun","Mar","Mier","Jue","Sab"};
         final DateFormatSymbols symbols = new DateFormatSymbols(); //use user locale
         final String nameDayOfWeek = shortWeekdaysxx[number]; //Short name or getWeekdays for complete name
         return nameDayOfWeek.toUpperCase(); //name to uppercase
